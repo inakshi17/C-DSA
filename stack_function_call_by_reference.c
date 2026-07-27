754aafc03c3aa1cd03946a6580de1b66dda4e426
Inakshi <inakshi.8734@gmail.com>
2026-07-27 21:55:42 +0530

    Implement stack operations using function calls

---
+++ stack_function_call_by_reference.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+# define MAX 10
+void push(int [], int *);
+void pop(int [],int *);
+void peek(int [], int);
+void display(int [], int);
+
+int main(){
+    int a[MAX], top=-1 , ch;
+    do{
+        printf("\n1-push\n2-pop\n3-peek\n4-display\n5-exit");
+        printf("\nenter your choice : ");
+        scanf("%d",&ch);
+        switch(ch){ 
+            case 1:
+            push(a, &top);
+            break;
+            case 2:
+            pop(a, &top);
+            break;
+            case 3:
+            peek(a, top);
+            break;
+            case 4:
+            display(a, top);
+            break;
+        }
+    }
+    while(ch>=1 && ch<=4);
+    return 0;
+}
+
+void push(int a[], int *top){
+    int z;
+    if(*top==MAX-1){
+        printf("stack is full");
+    }
+    else{
+        printf("enter new element : ");
+        scanf("%d",&z);
+        a[++(*top)]=z;
+    }
+}
+void pop(int a[], int *top){
+    int z;
+    if(*top==-1){
+        printf("stack is empty");
+    }
+    else{
+        z=a[(*top)--];
+        printf("deleted element : %d",z);
+    }
+}
+
+void peek(int a[], int top){
+    if(top==-1){
+        printf("stack is empty");
+    }
+    else{
+        printf("currently : %d",a[top]);
+    }
+}
+
+void display(int a[], int top){
+    int i=0;
+    if(top==-1){
+        printf("stack is empty");
+    }
+    else{
+        printf("\n");
+        while(i<top+1){
+            printf("%d ",a[i++]);
+        }
+    }
+    printf("\n");
+}
